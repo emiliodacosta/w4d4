5643eb68bc14dcf16a44f0f6048c164e4e91b653
Emilio da Costa <emidac@gmail.com>
2017-09-28 16:56:18 -0400

    Before Styling

--- music_app/app/controllers/SessionsController.rb
+++ music_app/app/controllers/SessionsController.rb
@@ -1,3 +1,28 @@
 class SessionsController <ApplicationController
+  before_action :require_no_user!, only: [:create, :new]
+
+  def create
+    user = User.find_by_credentials(
+      params[:user][:email],
+      params[:user][:password]
+    )
+
+    if user.nil?
+      flash.now[:errors] = ["Incorrect username and/or password"]
+      render :new
+    else
+      login_user!(user)
+      redirect_to user_url #TODO redirect_to bands_url
+    end
+  end
+
+  def new
+    render :new
+  end
+
+  def destroy
+    logout_user!
+    redirect_to new_session_url
+  end
 
 end
